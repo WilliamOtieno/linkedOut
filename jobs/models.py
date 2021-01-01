0b50c30262587ac9e279615c77e25b1ab43cde7b
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-01 22:27:48 +0300

    added models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Jobs(models.Model):
+    job_title = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=30)
+    county = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_num = models.CharField(max_length=12)
