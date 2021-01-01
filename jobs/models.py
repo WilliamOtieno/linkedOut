62ccabc054dca98bb4a80ae4cbca5bee5f3302c2
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-01 22:42:57 +0300

    renamed model

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Jobs(models.Model):
+class Job(models.Model):
     job_title = models.CharField(max_length=50)
     company_name = models.CharField(max_length=30)
     county = models.CharField(max_length=20)
