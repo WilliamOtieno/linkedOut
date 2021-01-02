a6a20a9b2e5b10e341bed5661fd34f246369bf75
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-02 22:40:49 +0300

    added desc row

--- jobs/models.py
+++ jobs/models.py
@@ -9,6 +9,7 @@ class Job(models.Model):
     county = models.CharField(max_length=20)
     email = models.EmailField()
     phone_num = models.CharField(max_length=12)
+    description = models.TextField(max_length=200)
 
     def __str__(self):
         return self.job_title
