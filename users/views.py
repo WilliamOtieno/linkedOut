d432a2ffe78da4ad4df5bac04cafc66dbaf7d46c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-04 22:06:15 +0300

    added profile view

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
