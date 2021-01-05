ee19f66e2220ca296355e1a139b86e2d31795804
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-05 11:17:23 +0300

    class-based view

--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
-
+from django.views.generic import ListView
 
 # Create your views here.
 
 
-def home(request):
+"""def home(request):
     context = {'jobs': Job.objects.all()}
-    return render(request, template_name='jobs/index.html', context=context)
+    return render(request, template_name='jobs/index.html', context=context)"""
 
+
+class JobListView(ListView):
+    model = Job
+    template_name = 'jobs/index.html'
+    context_object_name = 'jobs'
+    paginate_by = 5
