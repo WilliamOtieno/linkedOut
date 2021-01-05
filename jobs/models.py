4d50e803f08f93c6d0600f245a19b4aaa01e150c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-05 11:43:33 +0300

    moded models

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -10,6 +11,7 @@ class Job(models.Model):
     email = models.EmailField()
     phone_num = models.CharField(max_length=12)
     description = models.TextField(max_length=500)
+    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.job_title
