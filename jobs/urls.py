f580c15ab3bb44dc36029a87b2b1ef626b14cfc2
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-01-05 11:29:16 +0300

    moded urls

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import JobListView
 
 urlpatterns = [
-    path('', JobListView.as_view(), name='jobs-home'),
+    path('', JobListView.as_view(), name='home'),
 ]
